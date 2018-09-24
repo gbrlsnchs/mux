866c7267ea0a48cd057a12076a3b409e2dde1d77
Gabriel Sanches <g@lonemaker.com>
2018-09-24 20:29:31 -0300

    Use third-party radix tree package and change types

--- router.go
+++ router.go
@@ -1,16 +1,140 @@
 package mux
 
 import (
+	"context"
 	"net/http"
+	"strings"
+
+	"github.com/gbrlsnchs/radix"
 )
 
-// Router is a prefix router created from an already existent mux.
+// MiddlewareFunc is a middleware adapter.
+type MiddlewareFunc func(next http.Handler) http.Handler
+
+// Router is an HTTP multiplexer.
 type Router struct {
-	path []byte
-	m    *Mux
+	path        string
+	methods     map[string]*radix.Tree
+	fns         []MiddlewareFunc
+	ctxKey      interface{}
+	debug       bool
+	placeholder byte
 }
 
-// Handle handles an HTTP handler according to a method and a path.
+// New creates a new HTTP multiplexer.
+func New(path string) *Router {
+	return &Router{
+		path:        path,
+		methods:     make(map[string]*radix.Tree, 9),
+		placeholder: ':',
+	}
+}
+
+// Handle sets an HTTP request handler.
 func (rt *Router) Handle(method, path string, handler http.Handler) {
-	rt.m.handle(method, append(rt.path, path...), handler)
+	rt.handle(method, path, handler)
+}
+
+// HandleFunc sets an HTTP request handler function.
+func (rt *Router) HandleFunc(method, path string, handler http.HandlerFunc) {
+	rt.handle(method, path, handler)
+}
+
+// Router creates a subrouter.
+func (rt *Router) Router(path string) *Router {
+	return &Router{
+		path:    rt.path + path,
+		methods: rt.methods,
+		fns:     rt.fns,
+		ctxKey:  rt.ctxKey,
+		debug:   rt.debug,
+	}
+}
+
+// ServeHTTP implements the http.Handler interface by finding an endpoint in the trie.
+// If there are any parameters, they are set to the request's context.
+func (rt *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if rt.methods[r.Method] == nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	trie := rt.methods[r.Method]
+	if n, p := trie.Get(r.URL.Path); n != nil {
+		// Stores parameters in the request's context.
+		if len(p) > 0 && rt.ctxKey != nil {
+			ctx := r.Context()
+			r = r.WithContext(context.WithValue(ctx, rt.ctxKey, Params(p)))
+		}
+		if handler, ok := n.Value.(http.Handler); ok {
+			handler.ServeHTTP(w, r)
+			return
+		}
+	}
+	http.NotFound(w, r)
+}
+
+// SetCtxKey sets the key for accessing parameters in the request's context object.
+// The idea is to make this key unique, maybe using enums or a UUID.
+func (rt *Router) SetCtxKey(v interface{}) {
+	rt.ctxKey = v
+}
+
+// SetDebug sets the debug flag.
+func (rt *Router) SetDebug(debug bool) {
+	rt.debug = debug
+}
+
+// SetPlaceholder sets a different placeholder to be used in dynamic paths.
+func (rt *Router) SetPlaceholder(c byte) {
+	rt.placeholder = c
+	for k := range rt.methods {
+		rt.methods[k].SetBoundaries(rt.placeholder, '/')
+	}
+}
+
+func (rt *Router) String() string {
+	var bd strings.Builder
+	for k, v := range rt.methods {
+		bd.WriteString(k)
+		bd.WriteByte('\n')
+		bd.WriteString(v.String())
+	}
+	return bd.String()
+}
+
+// Use set middleware functions to run before each request.
+func (rt *Router) Use(fns ...MiddlewareFunc) {
+	rt.fns = fns
+}
+
+func (rt *Router) handle(method, path string, handler http.Handler) {
+	fpn := rt.resolvePath(path)
+	chain := Chain(rt.fns...)(handler)
+	m := rt.methods[method]
+	if m == nil {
+		flag := 0
+		if rt.debug {
+			flag = radix.Tdebug
+		}
+		m = radix.New(flag)
+		m.SetBoundaries(rt.placeholder, '/')
+		rt.methods[method] = m
+	}
+	m.Add(fpn, chain)
+	m.Sort(radix.PrioritySort)
+}
+
+func (rt *Router) resolvePath(path string) string {
+	var bd strings.Builder
+	bd.WriteString(rt.path)
+	bd.WriteString(path)
+	path = bd.String()
+	if path == "" {
+		return "/"
+	}
+	for len(path) > 1 && path[0] == '/' && path[1] == '/' {
+		path = path[1:]
+	}
+	return path
 }
--- router_test.go
+++ router_test.go
@@ -4,16 +4,256 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 
 	. "github.com/gbrlsnchs/mux"
 )
 
+const paramsKey = "params"
+
+var params Params
+
 func TestRouter(t *testing.T) {
 	testCases := []struct {
 		method   string
 		requests map[string]int
-		m        *Mux
+		rt       *Router
+		fns      []MiddlewareFunc
+		handlers map[string]http.Handler
+		params   map[string]Params
+	}{
+		{
+			method: http.MethodGet,
+			requests: map[string]int{
+				"/test": http.StatusOK,
+			},
+			rt: New("/"),
+			handlers: map[string]http.Handler{
+				"/test": http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.WriteHeader(http.StatusOK)
+				}),
+			},
+		},
+		{
+			method: http.MethodGet,
+			requests: map[string]int{
+				"/test": http.StatusOK,
+			},
+			rt: New("/test"),
+			handlers: map[string]http.Handler{
+				"": http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.WriteHeader(http.StatusOK)
+				}),
+			},
+		},
+		{
+			method: http.MethodPost,
+			requests: map[string]int{
+				"/test": http.StatusOK,
+			},
+			rt: New("/test"),
+			handlers: map[string]http.Handler{
+				"": http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.WriteHeader(http.StatusOK)
+				}),
+			},
+		},
+		{
+			method: http.MethodGet,
+			requests: map[string]int{
+				"/test": http.StatusNotFound,
+			},
+			rt: New("/"),
+			handlers: map[string]http.Handler{
+				"/testing": http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.WriteHeader(http.StatusOK)
+				}),
+			},
+		},
+		{
+			method: http.MethodGet,
+			requests: map[string]int{
+				"/test": http.StatusNotFound,
+			},
+			rt: New("/testing"),
+			handlers: map[string]http.Handler{
+				"/testing": http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.WriteHeader(http.StatusOK)
+				}),
+			},
+		},
+		{
+			method: http.MethodGet,
+			requests: map[string]int{
+				"/test": http.StatusOK,
+			},
+			rt: New("/"),
+			fns: []MiddlewareFunc{
+				func(next http.Handler) http.Handler {
+					return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						next.ServeHTTP(w, r)
+					})
+				},
+			},
+			handlers: map[string]http.Handler{
+				"/test": http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.WriteHeader(http.StatusOK)
+				}),
+			},
+		},
+		{
+			method: http.MethodGet,
+			requests: map[string]int{
+				"/test": http.StatusOK,
+			},
+			rt: New("/"),
+			fns: []MiddlewareFunc{
+				func(next http.Handler) http.Handler {
+					return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						next.ServeHTTP(w, r)
+					})
+				},
+				func(next http.Handler) http.Handler {
+					return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						next.ServeHTTP(w, r)
+					})
+				},
+			},
+			handlers: map[string]http.Handler{
+				"/test": http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.WriteHeader(http.StatusOK)
+				}),
+			},
+		},
+		{
+			method: http.MethodGet,
+			requests: map[string]int{
+				"/test": http.StatusBadRequest,
+			},
+			rt: New("/"),
+			fns: []MiddlewareFunc{
+				func(next http.Handler) http.Handler {
+					return http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+						w.WriteHeader(http.StatusBadRequest)
+					})
+				},
+			},
+			handlers: map[string]http.Handler{
+				"/test": http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.WriteHeader(http.StatusOK)
+				}),
+			},
+		},
+		{
+			method: http.MethodGet,
+			requests: map[string]int{
+				"/": http.StatusOK,
+			},
+			rt: New("/"),
+			handlers: map[string]http.Handler{
+				"/": http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.WriteHeader(http.StatusOK)
+				}),
+			},
+		},
+		{
+			method: http.MethodGet,
+			requests: map[string]int{
+				"/": http.StatusOK,
+			},
+			rt: New(""),
+			handlers: map[string]http.Handler{
+				"/": http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.WriteHeader(http.StatusOK)
+				}),
+			},
+		},
+		{
+			method: http.MethodGet,
+			requests: map[string]int{
+				"/": http.StatusOK,
+			},
+			rt: New("/"),
+			handlers: map[string]http.Handler{
+				"": http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.WriteHeader(http.StatusOK)
+				}),
+			},
+		},
+		{
+			method: http.MethodGet,
+			requests: map[string]int{
+				"/": http.StatusOK,
+			},
+			rt: New(""),
+			handlers: map[string]http.Handler{
+				"": http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.WriteHeader(http.StatusOK)
+				}),
+			},
+		},
+		{
+			method: http.MethodGet,
+			requests: map[string]int{
+				"/123":          http.StatusOK,
+				"/123/test/456": http.StatusNoContent,
+			},
+			rt: New("/"),
+			handlers: map[string]http.Handler{
+				"/:test": http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					params = r.Context().Value(paramsKey).(Params)
+					w.WriteHeader(http.StatusOK)
+				}),
+				"/:test/test/:testing": http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					params = r.Context().Value(paramsKey).(Params)
+					w.WriteHeader(http.StatusNoContent)
+				}),
+			},
+			params: map[string]Params{
+				"/123":          Params{"test": "123"},
+				"/123/test/456": Params{"test": "123", "testing": "456"},
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run("", func(t *testing.T) {
+			params = nil
+			tc.rt.SetDebug(true)
+			tc.rt.SetCtxKey(paramsKey)
+			tc.rt.Use(tc.fns...)
+
+			for path, handler := range tc.handlers {
+				tc.rt.Handle(tc.method, path, handler)
+			}
+			for path, status := range tc.requests {
+				t.Run(fmt.Sprintf("%s %s", tc.method, path), func(t *testing.T) {
+					w := httptest.NewRecorder()
+					r := httptest.NewRequest(tc.method, path, nil)
+
+					t.Log(tc.rt.String())
+					tc.rt.ServeHTTP(w, r)
+					if want, got := status, w.Code; want != got {
+						t.Errorf("want %d, got %d", want, got)
+					}
+					if want, got := len(tc.params[path]), len(params); want != got {
+
+					}
+					if len(tc.params) > 0 {
+						if want, got := tc.params[path], params; !reflect.DeepEqual(want, got) {
+							t.Errorf("want %#v, got %#v", want, got)
+						}
+					}
+				})
+			}
+		})
+	}
+}
+func TestSubrouter(t *testing.T) {
+	testCases := []struct {
+		method   string
+		requests map[string]int
+		rt       *Router
 		path     string
 		handlers map[string]http.Handler
 	}{
@@ -22,7 +262,7 @@ func TestRouter(t *testing.T) {
 			requests: map[string]int{
 				"/mux/router/test": http.StatusOK,
 			},
-			m:    New("/mux"),
+			rt:   New("/mux"),
 			path: "/router",
 			handlers: map[string]http.Handler{
 				"/test": http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
@@ -35,7 +275,7 @@ func TestRouter(t *testing.T) {
 			requests: map[string]int{
 				"/mux/router/test": http.StatusOK,
 			},
-			m:    New("/mux"),
+			rt:   New("/mux"),
 			path: "/router",
 			handlers: map[string]http.Handler{
 				"/test": http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
@@ -48,7 +288,7 @@ func TestRouter(t *testing.T) {
 			requests: map[string]int{
 				"/": http.StatusOK,
 			},
-			m:    New("/"),
+			rt:   New("/"),
 			path: "/",
 			handlers: map[string]http.Handler{
 				"/": http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
@@ -59,7 +299,8 @@ func TestRouter(t *testing.T) {
 	}
 	for _, tc := range testCases {
 		t.Run(fmt.Sprintf("%s %#v", tc.method, tc.requests), func(t *testing.T) {
-			rt := tc.m.Router(tc.path)
+			rt := tc.rt.Router(tc.path)
+			rt.SetDebug(true)
 			for path, handler := range tc.handlers {
 				rt.Handle(tc.method, path, handler)
 			}
@@ -67,7 +308,8 @@ func TestRouter(t *testing.T) {
 				w := httptest.NewRecorder()
 				r := httptest.NewRequest(tc.method, path, nil)
 
-				tc.m.ServeHTTP(w, r)
+				t.Log(tc.rt.String())
+				tc.rt.ServeHTTP(w, r)
 				if want, got := status, w.Code; want != got {
 					t.Errorf("want %d, got %d", want, got)
 				}
