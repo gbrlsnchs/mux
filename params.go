911f0c7595fb9c238a1b05a6464f937463e39ab1
Gabriel Sanches <g@lonemaker.com>
2018-10-03 20:03:21 -0300

    Resolve empty params in order to prevent panicking

--- params.go
+++ params.go
@@ -4,5 +4,8 @@ import "context"
 
 // Params is a helper to access URL parameters.
 func Params(ctx context.Context, ctxKey interface{}) map[string]string {
-	return ctx.Value(ctxKey).(map[string]string)
+	if p, ok := ctx.Value(ctxKey).(map[string]string); ok {
+		return p
+	}
+	return map[string]string{}
 }
